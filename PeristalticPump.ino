999f8ee7a69e38ac8f47953d6434e17ead7be386
Zachary Newell <zlantern@gmail.com>
2013-03-18 14:33:58 -0700

    Added support for two motors and sensor control
    I also added daisy chaining via I2C.

--- PeristalticPump.ino
+++ PeristalticPump.ino
@@ -1,15 +1,28 @@
+#include <EEPROM.h>
+#include <Wire.h>
 #include <FlexiTimer2.h>
 #include <DualMC33926MotorShield.h>
 #include <CmdMessenger.h>
-#include <Base64.h>
-#include <Streaming.h>
 
 DualMC33926MotorShield md;
 
+int I2CAddress     = 100;
 
-char field_separator = ',';
-char command_separator = ';';
-CmdMessenger cmdMessenger = CmdMessenger(Serial, field_separator, command_separator);
+int Sensor1Pin = 2;
+int Sensor2Pin = 3;
+
+int Sensor1Voltage;
+int Sensor2Voltage;
+
+boolean RunMotor1;
+boolean RunMotor2;
+
+boolean RenewMotor1;
+boolean RenewMotor2;
+
+char FieldSeparator = ',';
+char CommandSeparator = ';';
+CmdMessenger cmdMessenger = CmdMessenger(Serial, FieldSeparator, CommandSeparator);
 
 
 enum
@@ -24,9 +37,14 @@ enum
 
 messengerCallbackFunction messengerCallbacks[] = 
 {
-  turn_pump_on,    // 006 
-  turn_pump_off,   // 007
-  get_current,     // 008
+  turn_pump_on,        // 005 
+  turn_pump_off,       // 006
+  sensor_control,      // 007
+  renew_motor_timer,   // 008
+  set_i2c_address,     // 009
+  get_i2c_address,     // 010
+  send_i2c_message,    // 011
+  test,                // 012
   NULL
 };
 
@@ -47,15 +65,78 @@ void turn_pump_on()
       stopIfFault();
   }
 }
+void sensor_control(){
+  
+  char buf[350] = { '\0' };
+  cmdMessenger.copyString(buf, 350);
+  if(buf[0]){
+      int number = atoi( buf );
+      
+      if(number == 0){
+        FlexiTimer2::stop();
+      } else if ( number == 1){
+        FlexiTimer2::start();
+      }
+  }
+  cmdMessenger.sendCmd(kACK,"OK");
+}
 
-void get_current()
+void renew_motor_timer()
 {
-  int current = md.getM1CurrentMilliamps();
-  stopIfFault();
+  char buf[350] = { '\0' };
+  cmdMessenger.copyString(buf, 350);
+  if(buf[0]){
+      int number = atoi( buf );
+      if(number == 1){
+        RenewMotor1 = true;
+      } else if ( number == 2){
+        RenewMotor2 = true;
+      }
+  }
+}
+void set_i2c_address(){
+  char buf[350] = { '\0' };
+  cmdMessenger.copyString(buf, 350);
+  if(buf[0]){
+      int number = atoi( buf );
+      EEPROM.write(I2CAddress,number);
+  }
+  cmdMessenger.sendCmd(kACK,"Test OK");
+}
+
+void get_i2c_address(){
+  int address = EEPROM.read(I2CAddress);
+  Serial.print("Address: ");
+  Serial.println(address);
+}
+
+void send_i2c_message(){
+  int i2c_address = 0;
+  int i = 0;
+  while ( cmdMessenger.available() ){
+    
+    char buf[350] = { '\0' };
+    cmdMessenger.copyString(buf, 350);
+    if ((i==0) && (buf[0])){
+      int number = atoi( buf );
+      i2c_address = number;
+    } else if ((i>0) && (buf[0])){
+      int number = atoi( buf );
+      Wire.beginTransmission(i2c_address);
+      Wire.write(buf);
+      Serial.println(buf);
+      Wire.endTransmission();
+    }
+    i++;
+  }
+}
+
+void test(){
+  cmdMessenger.sendCmd(kACK,"Test OK");
 }
 
 
-void arduino_ready()
+void ready()
 {
   cmdMessenger.sendCmd(kACK,"Ready");
 }
@@ -80,50 +161,88 @@ void stopIfFault()
 {
   if (md.getFault())
   {
-	Serial.println(md.getFault());
+    
+    Serial.println(md.getFault());
     cmdMessenger.sendCmd(kHWERR,"Hardware Error");
   } else{
-	cmdMessenger.sendCmd(kACK,"OK");
+    cmdMessenger.sendCmd(kACK,"OK");
   }
 }
 
 void setup()
 {
+  int address = EEPROM.read(I2CAddress);
+  if(address==0){
+    Wire.begin();
+  } else {
+    Wire.begin(address);
+  }
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  
   Serial.begin(115200);
+  
+  //Serial CMDMessenger
   cmdMessenger.print_LF_CR();
-  cmdMessenger.attach(kARDUINO_READY, arduino_ready);
+  cmdMessenger.attach(kARDUINO_READY, ready);
   cmdMessenger.attach(unknownCmd);
-
-
+  
+  //Attach the callbacks
   attach_callbacks(messengerCallbacks);
-
   md.init();
+  
+  FlexiTimer2::set(1000,run_motor);
+  FlexiTimer2::start();
 }
 
-// Timeout handling
-long timeoutInterval = 2000; // 2 seconds
-long previousMillis = 0;
-int counter = 0;
-
-void timeout()
+void requestEvent()
 {
-  // blink
-  if (counter % 2)
-    digitalWrite(13, HIGH);
-  else
-    digitalWrite(13, LOW);
-  counter ++;
-}  
+  
+}
 
+void receiveEvent(int numBytes)
+{
+  cmdMessenger.field_separator = '-';
+  cmdMessenger.command_separator = '!';
+  while(Wire.available())    // slave may send less than requested
+  { 
+    int c = Wire.read();    // receive a byte as character
+    cmdMessenger.process(c);
+  }
+  cmdMessenger.field_separator = ',';
+  cmdMessenger.field_separator = ';';
+}
 
 void loop()
 {
   cmdMessenger.feedinSerialData();
-
-  // handle timeout function
-  if (  millis() - previousMillis > timeoutInterval )
-  {
-    timeout();
-    previousMillis = millis();
-  }
 }
+
+void read_sensor_values(){
+  Sensor1Voltage = analogRead(Sensor1Pin);
+  Sensor2Voltage = analogRead(Sensor2Pin);
+}
+
+void run_motor() {
+  
+  read_sensor_values();
+  
+   if(( RenewMotor1 == true) && (Sensor1Voltage < 1000)){
+     //Renew Motor 1
+     md.setM1Speed(400);
+   } else{
+     md.setM1Speed(0);
+   }
+  
+   if( (RenewMotor2 == true) && (Sensor2Voltage < 1000)){
+    //Renew Motor 2
+     md.setM2Speed(400);
+    } else {
+     md.setM2Speed(0);
+   }
+  
+  
+  RenewMotor1 = false ;
+  RenewMotor2 = false ;
+}
+
